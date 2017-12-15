853183b6f243049b37911392fdb48f6e6b0abaed
Hyleus <Hyleus@tuta.io>
2017-12-15 11:29:20 +0100

    Remove unused functions

--- src/main.cpp
+++ src/main.cpp
@@ -10,21 +10,6 @@
 #include "x.hpp"
 #include "image.hpp"
 
-template<typename Out>
-static void split(const std::string &s, char delim, Out result) {
-    std::stringstream ss;
-    ss.str(s);
-    std::string item;
-    while (std::getline(ss, item, delim)) {
-        *(result++) = item;
-    }
-}
-static std::vector<std::string> split(const std::string &s, char delim) {
-    std::vector<std::string> elems;
-    split(s, delim, std::back_inserter(elems));
-    return elems;
-}
-
 class MaimOptions {
 public:
     MaimOptions();
